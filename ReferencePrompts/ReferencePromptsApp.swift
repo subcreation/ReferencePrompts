0591c27c8f5806fca52695d72fabbe1079e19451
subcreation <in8media@gmail.com>
2023-01-09 15:01:21 -0800

    Initial Commit

---
+++ ReferencePrompts/ReferencePromptsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReferencePromptsApp.swift
+//  ReferencePrompts
+//
+//  Created by Nathanael Roberton on 1/9/23.
+//
+
+import SwiftUI
+
+@main
+struct ReferencePromptsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
